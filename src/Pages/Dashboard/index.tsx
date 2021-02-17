74db2172bb2bc1a0cbd070ef33c828098b825fce
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-17 09:38:23 -0300

    Dashboard template

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
+
+export default Dashboard;
