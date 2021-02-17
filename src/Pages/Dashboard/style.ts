2a770baae891451a0cf78ce4dd2db9a7054c36aa
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-17 15:15:04 -0300

    Adding dashboard style

---
+++ src/Pages/Dashboard/style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const DashMenu = styled.nav`
+  width: 100px;
+  height: 100vh;
+  position: fixed;
+  background-color: #68DE5A;
+`
+
+export const DashMain = styled.main`
+  
+`
+
+export const DashBoard = styled.div`
+  display: flex;
+`
