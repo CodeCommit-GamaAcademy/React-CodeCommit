1837b83f805283aecf128f93b96388b194217b3c
Igor Santos <igornzds@gmail.com>
2021-02-16 23:52:18 -0300

    Start login form page

---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '../../assets/main-logo.png';
+
+import { Container, Img, Form, FormTitle, FormInput, FormButtonSubmit } from './styles';
+
+const Login: React.FC = () => {
+  return (
+    <Container>
+      <Img src={Logo} alt="Logo principal" />
+
+
+      <Form>
+        <FormTitle>Faça o login</FormTitle>
+
+        <FormInput placeholder="Digite seu usuário" />
+        <FormInput placeholder="Digite sua senha" type="password" />
+
+        <FormButtonSubmit>
+          Continuar
+          <span>➔</span>
+        </FormButtonSubmit>
+
+        <Link to="#">Esqueci minha senha</Link>
+        <Link to="#">Ainda não sou cliente</Link>
+
+      </Form>
+    </Container>
+  );
+}
+
+export default Login;
---
+++ src/Pages/Login/styles.ts
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Img = styled.img`
+  position: absolute;
+  left: 65px;
+  height: 65px;
+  top: 58px;
+`;
+
+export const Form = styled.form`
+  padding: 40px 46px;
+  background: #fff;
+  min-height: 422px;
+  border-radius: 8px;
+  width: 455px;
+
+  a {
+    color: #8c52e5;
+    font-size: 13px;
+    display: block;
+    text-align: center;
+
+    & + a {
+      margin-top: 10px;
+    }
+  }
+`;
+
+export const FormTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 700;
+  text-align: center;
+`;
+
+export const FormInput = styled.input`
+  margin-top: 50px;
+  border: none;
+  border-bottom: 1px solid #878686;
+  width: 100%;
+  padding: 10px 14px;
+  font-size: 14px;
+
+  &::placeholder {
+    color: #878686;
+  }
+
+  &:focus {
+    border-bottom: 1px solid #68de5a;
+  }
+`;
+
+export const FormButtonSubmit = styled.button`
+  background: #68de5a;
+  margin-bottom: 38px;
+  margin-top: 36px;
+  width: 100%;
+  padding: 12px 35px;
+  display: flex;
+  color: #fff;
+  font-size: 16px;
+  border-radius: 20px;
+  border: none;
+  justify-content: space-between;
+`;
