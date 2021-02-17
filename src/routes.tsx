45c209a5e2c5397bd3dd010ecbd8dbf7cf1182b1
NoronhaGCP <inc.691@gmail.com>
2021-02-17 00:21:21 -0300

    add Recover Password route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './Pages/Landing';
+import RecoverPassword from './Pages/RecoverPassword';
 
 const Routes: React.FC = () => {
     return (
@@ -10,6 +11,7 @@ const Routes: React.FC = () => {
                 {/* Rotas */}
 
                 <Route path="/" exact component={ Landing } />
+                <Route path="/recover" component={ RecoverPassword } />
             </Switch>
         </BrowserRouter>
     ); 
