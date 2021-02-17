3a356ce246575c5168278e1231a0c0f3903a78b5
NoronhaGCP <inc.691@gmail.com>
2021-02-17 19:35:00 -0300

    add error to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './Pages/Landing';
 import Login from './Pages/Login';
 import RecoverPassword from './Pages/RecoverPassword';
+import Error from './Pages/Error';
 
 const Routes: React.FC = () => {
     return (
@@ -14,6 +15,7 @@ const Routes: React.FC = () => {
                 <Route path="/" exact component={Landing} />
                 <Route path="/login" component={Login} />
                 <Route path="/recover" component={RecoverPassword} />
+                <Route path="/error" component = { Error } />
             </Switch>
         </BrowserRouter>
     );
