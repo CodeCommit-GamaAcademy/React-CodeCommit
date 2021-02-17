8f8e8f0f0f92a04a7288afdd1302f84d969569a9
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-17 15:29:21 -0300

    Adding DashMain, DashBoard, DashNav styles

--- src/Pages/Dashboard/style.ts
+++ src/Pages/Dashboard/style.ts
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
 
 export const DashMenu = styled.nav`
-  width: 100px;
+  width: 110px;
   height: 100vh;
   position: fixed;
   background-color: #68DE5A;
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 90px;
+    height: 90px;
+    margin-top: 10px;
+  }
 `
 
 export const DashMain = styled.main`
-  
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
 `
 
 export const DashBoard = styled.div`
