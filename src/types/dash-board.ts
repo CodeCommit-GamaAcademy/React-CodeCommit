922e7cdcd47aca361c4fc86df3b3807e9725ecdc
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-18 17:37:25 -0300

    setting dash-board types

---
+++ src/types/dash-board.ts
@@ -0,0 +1,23 @@
+export interface Conta {
+  id: number,
+  lancamentos: Lancamento[],
+  saldo: number,
+}
+
+export interface Lancamento {
+  conta: number,
+  data: string,
+  descricao: string,
+  id: number,
+  planoConta: Plano,
+  tipo: string,
+  valor: number,
+}
+
+export interface Plano {
+  descricao: string,
+  id: number,
+  login: string,
+  padrao: boolean,
+  tipoMovimento: string,
+}
