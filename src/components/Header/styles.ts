22f292b66a5c79950120bf149e0ea6c48f13b3d2
Igor Santos <igornzds@gmail.com>
2021-02-18 00:30:11 -0300

    create header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const MainHeader = styled.header``;
+
+export const MainHeaderContainer = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  width: 100%;
+  padding: 0 16px;
+`;
