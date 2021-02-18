3d58f6a6c734a9508571a7592f0332e38eb6e293
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-18 17:37:59 -0300

    removing dash-board types from user.ts

--- src/types/user.ts
+++ src/types/user.ts
@@ -26,27 +26,3 @@ export interface UserResponse {
       senhaTemporaria: string
     }
 }
-
-export interface Conta {
-  id: number,
-  lancamentos: Lancamento[],
-  saldo: number,
-}
-
-export interface Lancamento {
-  conta: number,
-  data: string,
-  descricao: string,
-  id: number,
-  planoConta: Plano,
-  tipo: string,
-  valor: number,
-}
-
-export interface Plano {
-  descricao: string,
-  id: number,
-  login: string,
-  padrao: boolean,
-  tipoMovimento: string,
-}
