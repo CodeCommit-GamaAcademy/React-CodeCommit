7ae24c83e69b6bab3ff2e829ccc5f1d76f345d7e
NoronhaGCP <inc.691@gmail.com>
2021-02-18 17:36:05 -0300

    add user Filtered types

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,13 @@
+interface User {
+  cpf: string,
+  id: number,
+  login: string,
+  nome: string,
+  redefinirSenha: boolean,
+  senha: string,
+  senhaTemporaria: string
+}
+
 export interface UserResponse {
     conta: {
       descricao: string,
@@ -16,13 +26,25 @@ export interface UserResponse {
     dataFim: Date,
     dataInicio: Date,
     token: string,
-    usuario: {
-      cpf: string,
+    usuario: User;
+}
+
+export interface FilteredUser {
+    conta: {
+      descricao: string,
       id: number,
-      login: string,
-      nome: string,
-      redefinirSenha: boolean,
-      senha: string,
-      senhaTemporaria: string
-    }
+      numero: string,
+      saldo: number,
+      tipo: "CB"
+    },
+    contaCredito: {
+      descricao: string,
+      id: number,
+      numero: string,
+      saldo: number,
+      tipo: "CB"
+    },
+    dataFim: Date,
+    dataInicio: Date,
+    usuario: Omit<Omit<User, "senha">, "senhaTemporaria">;
 }
