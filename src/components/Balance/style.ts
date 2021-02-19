f54508f59fffde32988a92d28e1091c52ff9bfe7
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-19 12:38:11 -0300

    making a hide style animation and style

--- src/components/Balance/style.ts
+++ src/components/Balance/style.ts
@@ -29,6 +29,22 @@ export const BalanceItem = styled.div`
     color: #1783E7;
   }
 
+  .hide {
+    text-indent: 100%;
+    white-space: nowrap;
+    overflow: hidden;
+    background: #8C52E5;
+    @keyframes glow {
+      from {
+        background: #68DE5A;
+      }
+      to {
+        background: #8C52E5;
+      }
+}
+    animation: glow 2s infinite alternate;
+  }
+
   img {
     width: 27px;
     height: 27px;
@@ -69,4 +85,38 @@ export const BalanceContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
+`
+
+export const DashNameSection = styled.div`
+  color: white;
+  display: flex;
+  align-self: center;
+  justify-content: space-evenly;
+  margin: 20px 0;
+  margin-top: 40px;
+  width: 695px;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    div {
+      color: #8C52E5;
+      background: #68DE5A;
+      height: 40px;
+      width: 40px;
+      border-radius: 50%;
+      padding: 8px;
+      transition: 1s;
+      :hover {
+        cursor: pointer;
+        opacity: 0.8;
+      }
+    };
+  }
+
+  @media screen and (max-width: 900px){
+    width: 100%;
+  }
 `
