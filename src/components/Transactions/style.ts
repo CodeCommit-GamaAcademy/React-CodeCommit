e16fd046853b2194cccf7b9fa2224bd6d4754800
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-02-19 16:42:22 -0300

    setting transactions style

--- src/components/Transactions/style.ts
+++ src/components/Transactions/style.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
 export const TransactionsContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-left: 50px;
+  margin-right: 50px;
 
+  @media screen and (max-width: 900px){
+    width: 100%;
+    height: 100%;
+    margin: 10px;
+  }
 `
