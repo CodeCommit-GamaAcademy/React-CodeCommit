122bf4ab7422d6c7addd56355fc5567e2e38a2d3
NoronhaGCP <inc.691@gmail.com>
2021-02-19 17:51:33 -0300

    implement token expiration validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './routes';
+import { checkIsAuth } from './services/getIsAuth';
 
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
+  useEffect( () => {
+    checkIsAuth();
+  }, []);
+
   return (
     <>
       <Routes />
