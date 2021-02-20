769c459759efe193019ea68c683245fee3d30096
NoronhaGCP <inc.691@gmail.com>
2021-02-19 23:28:58 -0300

    sync localStorage to redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import Routes from './routes';
+
 import { checkIsAuth } from './services/getIsAuth';
+import updateState from './services/updateState';
 
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   useEffect( () => {
     checkIsAuth();
+    updateState();
   }, []);
 
   return (
