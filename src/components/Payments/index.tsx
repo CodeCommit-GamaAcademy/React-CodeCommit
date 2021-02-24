304bc0378b1ed63426777256a3f42dc653eda876
Igor Santos <igornzds@gmail.com>
2021-02-24 11:44:26 -0300

    apply unform Payments page

--- src/components/Payments/index.tsx
+++ src/components/Payments/index.tsx
@@ -1,30 +1,31 @@
-import React, { FormEvent, useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import { Form } from '@unform/web';
 import { FaArrowRight } from 'react-icons/fa'
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import { MdCached } from 'react-icons/md';
 
-import { PaymentsContainer, Form, FormInput, Button } from './style';
+import { PaymentsContainer, Button } from './style';
 import { ApplicationStore } from '../../store';
 import api from '../../services/api';
 import { Contas, Plano } from '../../types/dash-board';
+import Input from '../Input';
 
 interface PaymentsProps {
   func: Function;
 }
 
-const Payments: React.FC<PaymentsProps> = ( props ) => {
+const Payments: React.FC<PaymentsProps> = (props) => {
 
-  const [ loaded, setLoaded ] = useState(true);
-  const [ destinatario, setDestinatario ] = useState('');
-  const [ data, setData ] = useState('');
-  const [ descricao, setDescricao ] = useState('');
-  const [ valor, setValor ] = useState(0);
-  
-  const store = useSelector( (state: ApplicationStore) => state.user );
-  
-  const handleSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const [loaded, setLoaded] = useState(true);
+  const [destinatario, setDestinatario] = useState('');
+  const [data, setData] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [valor, setValor] = useState(0);
+
+  const store = useSelector((state: ApplicationStore) => state.user);
+
+  const handleSubmit = useCallback(async (dataProps: object) => {
     setLoaded(false);
     const date = new Date();
     const referenceDate = new Date(date.setDate(date.getDate() - 1));
@@ -38,27 +39,33 @@ const Payments: React.FC<PaymentsProps> = ( props ) => {
       setLoaded(true);
       return toast.error('Escolha outra data');
     }
-    if (descricao.length <= 3) {
+    if (descricao.length < 3) {
       setLoaded(true);
       return toast.error('Descrição não pode ser nula');
-    } 
+    }
     if (valor <= 0) {
       setLoaded(true);
-      return toast.error('Valor para transferencia deve ser maior que 0');    
+      return toast.error('Valor para transferencia deve ser maior que 0');
     }
-    
+
     try {
       const result = await api.get<Contas>(`/dashboard?fim=2021-02-22&inicio=2021-02-22&login=${store?.login}`, {
         headers: {
           Authorization: store?.token,
         }
       });
-      
+
       const resultPlan = await api.get<Plano[]>(`/lancamentos/planos-conta?login=${store?.login}`, {
         headers: {
           Authorization: store?.token,
         }
       });
+
+      if (result.data.contaBanco.saldo < valor) {
+        toast.error('Saldo insuficiente.');
+        setLoaded(true);
+        return;
+      }
 
       const { status } = await api.post('/lancamentos', {
         "conta": result.data.contaBanco.id,
@@ -84,7 +91,7 @@ const Payments: React.FC<PaymentsProps> = ( props ) => {
     setLoaded(true);
   }, [destinatario, data, descricao, valor, store?.login, store?.token]);
 
-  if(loaded) {
+  if (loaded) {
     return (
       <>
         <PaymentsContainer>
@@ -92,24 +99,24 @@ const Payments: React.FC<PaymentsProps> = ( props ) => {
             <p>
               Informe os dados para realizar sua transferência
             </p>
-  
-            <FormInput value={ destinatario } onChange={ e => setDestinatario(e.target.value) } type="text" placeholder="Login do destinatário" />
-            <FormInput value={ data } onChange={ e => setData(e.target.value) } type="date" />
-            <FormInput value={ descricao } onChange={ e => setDescricao(e.target.value) } type="text" placeholder="Descrição" />
-            <FormInput value={ valor } onChange={ e => setValor(Number(e.target.value)) } type="text" placeholder="Qual o valor de sua transferência?" />
-  
+
+            <Input name="receiver" value={destinatario} onChange={e => setDestinatario(e.target.value)} type="text" placeholder="Login do destinatário" />
+            <Input name="date" value={data} onChange={e => setData(e.target.value)} type="date" />
+            <Input name="description" value={descricao} onChange={e => setDescricao(e.target.value)} type="text" placeholder="Descrição" />
+            <Input name="transferValur" value={valor} onChange={e => setValor(Number(e.target.value))} type="text" placeholder="Qual o valor de sua transferência?" />
+
             <Button type="submit">
               <span>Transferir agora</span>
-              <FaArrowRight color="#8c52e5"/>
+              <FaArrowRight color="#8c52e5" />
             </Button>
           </Form>
-  
-  
+
+
         </PaymentsContainer>
       </>
     );
   } else {
-    return <MdCached color="#f0f0f0" size={ 200 } />
+    return <MdCached color="#f0f0f0" size={200} />
   }
 }
 
--- src/components/Payments/style.ts
+++ src/components/Payments/style.ts
@@ -1,75 +1,57 @@
 import styled from 'styled-components';
 
 export const PaymentsContainer = styled.div`
-    background: #fff;
-    border-radius: 8px;
-    width: 70%;
+  background: #fff;
+  border-radius: 8px;
+  width: 70%;
 
-    @media screen and (max-width: 900px) {
-        margin-top: 50px;
-        width: 90%;
-
-        p {
-        font-size: 14px;
-        font-weight: 500;
-        }
-    }
-`
-
-export const Form = styled.form`
+  form {
     min-height: 422px;
     padding: 40px 46px;
     width: 100%;
 
     p {
-        font-size: 24px;
-        font-weight: 500;
+      font-size: 24px;
+      font-weight: 500;
     }
 
     @media screen and (max-width: 900px) {
-        height: 100%
+      height: 100%;
     }
-`;
+  }
 
-export const FormInput = styled.input`
-    border: none;
-    border-bottom: 2px solid #878686;
-    color: #495057;
-    font-size: 14px;
-    margin-top: 20px;
-    padding: 10px 14px;
-    width: 100%;
+  @media screen and (max-width: 900px) {
+    margin-top: 50px;
+    width: 90%;
 
-    &::placeholder {
-        color: #878686;
+    p {
+      font-size: 14px;
+      font-weight: 500;
     }
-
-    &:focus {
-        border-bottom: 2px solid #8c52e5;
-    }
+  }
 `;
 
 export const Button = styled.button`
-    align-items: center;
-    background: #FFF;
-    border: 2px solid #8c52e5;
-    border-radius: 30px;
-    display: flex;
-    justify-content: space-between;
-    margin-top: 38px;
-    transition: .3s;
-    padding: 15px 40px;
-    width: 100%;
+  align-items: center;
+  background: #fff;
+  border: 2px solid #8c52e5;
+  border-radius: 30px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 38px;
+  transition: 0.3s;
+  padding: 15px 40px;
+  width: 100%;
 
-    span {
-        color: #8c52e5;
-        font-size: 16px;
-    }
+  span {
+    color: #8c52e5;
+    font-size: 16px;
+  }
 
-    &:hover {
-        background-color: #68DE5A;
-    }
-`
+  &:hover {
+    background-color: #68de5a;
+  }
+`;
 
 export const DashNameSection = styled.div`
   color: white;
@@ -77,7 +59,7 @@ export const DashNameSection = styled.div`
   margin-top: 40px;
   width: 70%;
 
-  @media screen and (max-width: 900px){
+  @media screen and (max-width: 900px) {
     width: 100%;
   }
-`
+`;
