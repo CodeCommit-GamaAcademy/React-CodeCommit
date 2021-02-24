4c77294722ad3d3619e2d21cd46fc08d90b94c53
Igor Santos <igornzds@gmail.com>
2021-02-24 11:55:23 -0300

    update Lauchs function

--- src/components/Extract/index.tsx
+++ src/components/Extract/index.tsx
@@ -17,51 +17,49 @@ enum PlanosConta {
     TU = 'Tranferência entre usuários'
 }
 
-const Extract: React.FC<ExtractData> = ( props ) => {
-    const [ allLauchs, setAllLaunchs ] = useState( () => {      
+const Extract: React.FC<ExtractData> = (props) => {
+    const [allLauchs, setAllLaunchs] = useState(() => {
         if (props.contaBanco?.lancamentos && props.contaCredito?.lancamentos) {
-            return [...props.contaBanco.lancamentos, ...props.contaCredito.lancamentos]
-        }else {
+            const initLaunchs = [...props.contaBanco.lancamentos, ...props.contaCredito.lancamentos];
+
+            const orderedLauchs = initLaunchs.slice().sort((a, b) => {
+                return Number(new Date(a.data)) - Number(new Date(b.data));
+            }).reverse();
+
+            return orderedLauchs;
+        } else {
             return [];
         }
     });
 
-    useEffect( () => {
-        const orderedLauchs = allLauchs.slice().sort( (a, b) => {
-            return Number(new Date(a.data)) - Number(new Date(b.data));
-        }).reverse();
-        
-        setAllLaunchs(orderedLauchs);
-    }, []);
-
     function typePlans(typePlan: string) {
-        if(typePlan === 'R') {
+        if (typePlan === 'R') {
             return PlanosConta.R;
-        } else if(typePlan === 'D') {
+        } else if (typePlan === 'D') {
             return PlanosConta.D;
-        } else if(typePlan === 'TC') {
+        } else if (typePlan === 'TC') {
             return PlanosConta.TC;
         } else {
             return PlanosConta.TU;
         }
     }
 
-    return(
+    return (
         <>
             <ExtractContainer>
                 <div className="title-container">
-                    <img src={currentIcon} alt="current icon"/>
+                    <img src={currentIcon} alt="current icon" />
                     <p>Últimos lançamentos</p>
                 </div>
                 {allLauchs.length === 0 && 'Nenhum lancamento'}
-                {allLauchs && allLauchs.map( (launch, index) => {
+                {allLauchs && allLauchs.map((launch, index) => {
                     return (
                         <ExtractItem key={index}>
-                            <img src={creditCardsIcon} alt="credit cards icon"/>
+                            <img src={creditCardsIcon} alt="credit cards icon" />
                             <div className="text-items">
                                 <strong>{typePlans(launch.planoConta.tipoMovimento)}</strong>
                                 <p>{launch.descricao}</p>
-                                <strong>{launch.valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</strong>
+                                <strong>{launch.valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</strong>
                             </div>
                             <p className="date">{launch.data}</p>
                         </ExtractItem>
