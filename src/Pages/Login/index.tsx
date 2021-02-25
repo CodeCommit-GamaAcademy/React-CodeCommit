5bbfb5ab17f5725fdb3152c9b2d3193268438caa
Igor Santos <igornzds@gmail.com>
2021-02-25 18:15:31 -0300

    update login page form validation with Yup

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { FaArrowRight } from 'react-icons/fa';
 import { Form } from '@unform/web';
+import * as yup from 'yup';
 
 import { Container, FormTitle } from './styles';
 import Button from '../../components/Button';
@@ -12,28 +13,34 @@ import { UserResponse } from '../../types/user';
 import updateReduxState from '../../services/updateReduxState';
 import Loader from '../../components/Loader';
 import Input from '../../components/Input';
+import { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 const Login: React.FC = () => {
-  const history = useHistory();
-
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
+  const history = useHistory();
+  const formRef = useRef<FormHandles>(null);
 
   const handleSubmit = useCallback(async (data: object) => {
     setLoading(true);
-
-    // Validation
-    // TODO
-
-    // API Request
     try {
-      const { data: response, status } = await api.post<UserResponse>('/login', {
+      formRef.current?.setErrors({});
+
+      const schema = yup.object().shape({
+        username: yup.string().required('Nome de usuário obrigatório '),
+        password: yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false
+      })
+
+      const { data: response } = await api.post<UserResponse>('/login', {
         "usuario": username,
         "senha": password
       });
-
-      if (status !== 200) throw new Error('Something went wrong with request');
 
       localStorage.setItem('@token_user', response.token);
       localStorage.setItem('@user_name', response.usuario.nome);
@@ -42,8 +49,11 @@ const Login: React.FC = () => {
       history.push('/dashboard');
     }
     catch (err) {
-      console.log(err);
-      history.push('/error');
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+      if (Object.keys(err).includes('isAxiosError')) {
+        history.push('/error');
+      }
     } finally {
       setLoading(false);
     }
@@ -54,10 +64,10 @@ const Login: React.FC = () => {
     <>
       <Header />
       <Container>
-        <Form onSubmit={handleSubmit} >
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <FormTitle>Faça o login</FormTitle>
 
-          <Input name="username" value={username} onChange={e => setUsername(e.target.value)} placeholder="Digite seu usuário" />
+          <Input name="username" value={username} onChange={e => setUsername(e.target.value)} placeholder="Digite seu usuário" autoFocus />
           <Input name="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Digite sua senha" type="password" />
 
           {loading ? <Loader /> : <Button
