5bb7b34a21b427f9f63c8bffd582d71f55438183
NoronhaGCP <inc.691@gmail.com>
2021-02-27 10:40:36 -0300

    set plans_data and transactions_data to redux

--- src/store/dashboard/actions.ts
+++ src/store/dashboard/actions.ts
@@ -1,4 +1,9 @@
 import { action } from 'typesafe-actions';
-import { DashboardActions, Screen } from './types';
+import { Plano } from '../../types/dash-board';
+import { DashboardActions, Screen, Transactions } from './types';
 
 export const change_screen = (screen: Screen) => action( DashboardActions.CHANGE_SCREEN, screen );
+
+export const insert_transaction_data = (data: Transactions) => action( DashboardActions.INSERT_TRANSACTION_DATA, data );
+
+export const insert_plans_data = (data: Plano[]) => action( DashboardActions.INSERT_PLANS_DATA, data );
--- src/store/dashboard/index.ts
+++ src/store/dashboard/index.ts
@@ -1,5 +1,6 @@
 import { Reducer } from "redux";
-import { DashboardActions, DashboardData, Screen } from "./types";
+import { Plano } from "../../types/dash-board";
+import { DashboardActions, DashboardData, Screen, Transactions } from "./types";
 
 const INITIAL_STATE: DashboardData = {
     current_screen: 'Transações'
@@ -10,7 +11,17 @@ const reducer: Reducer<DashboardData> = (state = INITIAL_STATE, action) => {
         case DashboardActions.CHANGE_SCREEN:
             const screenToChange = action.payload as Screen;
 
-            return { current_screen: screenToChange };
+            return { ...state, current_screen: screenToChange };
+
+        case DashboardActions.INSERT_TRANSACTION_DATA:
+            const dataToChange = action.payload as Transactions;
+
+            return { ...state, transactions_data: dataToChange };
+
+        case DashboardActions.INSERT_PLANS_DATA:
+            const plasnToChange = action.payload as Plano[];
+
+            return { ...state, plans_data: plasnToChange };
 
         default:
             return state;
