07370821b4f39095440a1f4b7346daa8c227db0e
NoronhaGCP <inc.691@gmail.com>
2021-02-28 22:35:41 -0300

    fix position of menu modal

--- src/Pages/Dashboard/style.ts
+++ src/Pages/Dashboard/style.ts
@@ -143,14 +143,16 @@ export const LogOutButton = styled.button`
 `
 export const ContainerMobile = styled.div`
 padding: 4em;
-position: absolute;
-top: 80px;
+position: fixed;
+top: 0;
+left: 0;
 z-index: 9999;
 background-color: rgba(0, 0, 0, 0.7);
 height: 100vh;
 width: 100%;
 display: flex;
 flex-direction: column;
+justify-content: center;
 
 div { 
   margin-bottom: 0.5em;
